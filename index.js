7ae7fab75d31f65ff51ff7eaf2d43ff1f80fd5fd
Ab Momin <mominurrahman10748@gmail.com>
2025-11-13 21:41:28 +0600

    check done

--- index.js
+++ index.js
@@ -10,12 +10,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// database user's credentials --->  mominurrahman017019_db_user  _ veHeE587FOH9eN1v
 
-const uri =
-  "mongodb+srv://mominurrahman017019_db_user:veHeE587FOH9eN1v@cluster0.mz4jlxs.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mz4jlxs.mongodb.net/?appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -30,7 +27,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
